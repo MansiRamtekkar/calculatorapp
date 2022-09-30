ebcfccb88ea0868af2dd8b270fb8cb9e69d65e71
Mansi Ramtekkar <mansiramtekkar@Mansis-MacBook-Air.local>
2022-09-30 14:59:52 +0530

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import Footer from "./components/Footer";
 
 function App() {
+
+  const [calc,setCalc] = useState('');
+  const [result, setResult] = useState('');
+
+  const ops = ['/','*','-','+'];
+
+  const updateCalc = value => {
+    if ( 
+      (ops.includes(value) && calc === '') || 
+      (ops.includes(value) && ops.includes(calc.slice(-1))) 
+      )
+      //  if the last entered value is an operator & calculation has nothing OR 
+      // if the last entered value is an operator & its previous value is also an operator, THEN return
+    {
+      return;
+    }
+
+    setCalc(calc + value);
+
+    if (!ops.includes(value)){
+      setResult(eval(calc + value).toString());
+    }
+
+  }
+
+  const createDigits = () => {
+    const digits = [];
+
+    for (let i=1 ; i < 10 ; i++) {
+      digits.push(
+        <button 
+          key={i}
+          onClick={() => updateCalc(i.toString())} >
+          {i}
+        </button>
+      )
+    }
+
+    return digits;
+  }
+
+  const calculate = () => {
+    setCalc(eval(calc).toString());
+    setResult(eval(calc).toString());
+  }
+
+  const deleteLast = () => {
+    if (calc === '') {
+      return;
+    }
+    const value = calc.slice(0, -1);
+    setCalc(value);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+
+        <div className="history">
+          <span className="history">{ calc || '0' }
+        </span></div>
+
+        <div className="display">
+          {result ? <span className="ans">{result}</span> : '0'}
+          {/* &nbsp; */}
+          {/* <span className="history">({ calc || "0" })</span> */}
+        </div>
+
+        <div className="operators">
+          <button onClick={ () => updateCalc('/')}><i class="fa-solid fa-divide"></i></button>
+          <button onClick={ () => updateCalc('*')}><i class="fa-solid fa-xmark"></i></button>
+          <button onClick={ () => updateCalc('-')}><i class="fa-solid fa-minus"></i></button>
+          <button onClick={ () => updateCalc('+')}><i class="fa-solid fa-plus"></i></button>
+          <button id="del" onClick={deleteLast}><i class="fa-solid fa-delete-left"></i></button>
+        </div>
+
+        <div className="digits">
+          { createDigits() }
+          <button onClick={ () => updateCalc('0')}>0</button>
+          <button onClick={ () => updateCalc('.')}>.</button>
+          <button id="eq" onClick={calculate}><i class="fa-solid fa-equals"></i></button>
+        </div>
+
+      </div>
+      <Footer />
     </div>
   );
+
 }
 
 export default App;
